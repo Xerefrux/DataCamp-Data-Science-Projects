5a95a47c8a0d0b04460ce13cc837c9e4c1c68b7a
Xerefrux <raiyansayeed0@gmail.com>
2026-03-22 21:16:17 +0600

    This is the 5th commit.

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 def error_messsage_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     filename = exc_tb.tb_frame.f_code.co_filename
